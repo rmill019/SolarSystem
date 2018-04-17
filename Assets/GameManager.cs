5d30367802135eae7d4d03e5e98799aee0dd4609
DESKTOP-OG1U6QC\rmill <rmill013@gmail.com>
2018-04-17 09:10:41 -0400

    Adds first Iteration Orbiting System

---
+++ Assets/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum E_Planets {e_Mercury = 0, e_Venus, e_Earth, e_Mars, e_Jupiter, e_Saturn, e_Uranus, e_Neptune };
+public class GameManager : MonoBehaviour {
+
+	public static GameManager		S;
+	// This array holds the number of days that it takes planets 1 - 8 to orbit the sun
+	public ushort[] 				daysToOrbit = new ushort[] { 88, 225, 365, 687, 4333, 10756, 30687, 60190};
+	public Transform				Sun;
+
+	void Awake () {
+		if (!S)
+			S = this;
+		else
+			Destroy (this.gameObject);
+
+		DontDestroyOnLoad (S);
+	}
+}
